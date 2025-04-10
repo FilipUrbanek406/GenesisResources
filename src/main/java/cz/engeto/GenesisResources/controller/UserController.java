b5822868f4d73c711646bbab8ea6bb42c8e6ee35
FilipUrbanek406 <filipurbanek406@gmail.com>
2025-04-10 10:34:37 +0200

    Vytvořen jednoduchá endpoint addUser, je potřeba doimplementovat další požadavky dle zadání

--- src/main/java/cz/engeto/GenesisResources/controller/UserController.java
+++ src/main/java/cz/engeto/GenesisResources/controller/UserController.java
@@ -5,10 +5,7 @@ import cz.engeto.GenesisResources.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +16,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        userService.addUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
-        return new ResponseEntity(users, HttpStatus.OK);
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
--- src/main/java/cz/engeto/GenesisResources/service/UserService.java
+++ src/main/java/cz/engeto/GenesisResources/service/UserService.java
@@ -3,9 +3,7 @@ package cz.engeto.GenesisResources.service;
 import cz.engeto.GenesisResources.model.User;
 import cz.engeto.GenesisResources.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +13,10 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
 
     public List<User> getAllUsers() {
 
